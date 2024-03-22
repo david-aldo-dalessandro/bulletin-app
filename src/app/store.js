267b4f3f919932b4d859825050895bbf75e842ec
David D'Alessandro <david.aldo.dalessandro@gmail.com>
2024-03-22 13:55:54 -0500

    Removed counter for master template

--- src/app/store.js
+++ src/app/store.js
@@ -5,10 +5,7 @@
  */
 
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: {},
 });
